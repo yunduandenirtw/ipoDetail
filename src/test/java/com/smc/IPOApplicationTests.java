721994b77d1b4db1ab39ebd1da8248132fb336e8
Catherine <wangrt@cn.ibm.com>
2019-12-15 22:25:28 +0800

    public

--- src/test/java/com/smc/IPOApplicationTests.java
+++ src/test/java/com/smc/IPOApplicationTests.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class IPOApplicationTests {
+public class IPOApplicationTests {
 
 //	@Autowired
 //	IpoService service;
